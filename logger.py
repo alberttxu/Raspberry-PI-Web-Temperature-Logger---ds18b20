8af75329dcabec7ce40f2ec7c26d7462665cec7c
alberttxu <albert.t.xu@gmail.com>
2016-07-19 10:15:38 -0400

    Add files via upload

---
+++ logger.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+from time import strftime 
+from therm import read_temp
+
+log_file = "/var/www/html/temp_data.dat"
+
+if __name__ == '__main__':
+    temp = read_temp()
+    with open(log_file, 'a') as f:
+        f.write(strftime("%H:%M " + temp + '\n'))
