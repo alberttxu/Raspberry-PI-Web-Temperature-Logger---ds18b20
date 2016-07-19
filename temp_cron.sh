bf43ebb6d63663d971e6e2eaab741e9663631f1b
alberttxu <albert.t.xu@gmail.com>
2016-07-19 10:00:55 -0400

    Add files via upload

---
+++ temp_cron.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+python /home/pi/bin/logger.py
+/home/pi/bin/plot_temp.sh
+
+hour=$(date +%H)
+minute=$(date +%M)
+
+if [ $hour == 23 ] && [ $minute == 59 ]; then
+    cp /var/www/html/temp.png /var/www/html/logs/`date +%Y-%m-%d`.png
+    truncate -s 0 /var/www/html/temp_data.dat
+fi
